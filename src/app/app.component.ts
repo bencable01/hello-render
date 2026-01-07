851295e69579bbaef0f8da75fe8e8d9b30a44aa9
bencable01 <bcable.development@gmail.com>
2026-01-06 22:55:46 -0500

    Test API call working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-render';
+  username: string = '';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+      this.http.get('http://localhost:3000/api/users').subscribe(result => {
+        var brctodo = 43;
+        this.username = result.toString();
+      });
+  }
 }
